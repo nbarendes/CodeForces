fd0a3f77eca440b6e74f311e23cff126ddab983f
nbarendes <nbarendes@gmail.com>
2021-03-21 12:14:00 +0100

    Move_and_Turn

---
+++ 1459B-Move_and_Turn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  
+    
+    int N;
+    cin>>N;
+	cout << (N % 2 + 1) * (N / 2 + 1) * ((N + 1) / 2 + 1)<<'\n';
+ 
+	
+ 
+ 
+    return 0;
+}
